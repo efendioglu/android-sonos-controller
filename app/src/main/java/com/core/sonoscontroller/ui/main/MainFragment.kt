77708266b932681e58342ab4f355c09ad29d6c52
Mehmet Kemal Efendioğlu <kemal@Mehmets-MacBook-Pro.local>
2021-11-24 12:54:53 +0300

    chore: edit dependencies

--- app/src/main/java/com/core/sonoscontroller/ui/main/MainFragment.kt
+++ app/src/main/java/com/core/sonoscontroller/ui/main/MainFragment.kt
@@ -1,12 +1,19 @@
 package com.core.sonoscontroller.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.core.sonoscontroller.R
+import com.core.sonoscontroller.sonos.SonosDevice
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainFragment : Fragment() {
 
@@ -18,13 +25,35 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+
+
+
+
         return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.discoverDevices()
+
+
+        viewModel.devicesLiveData.observe(viewLifecycleOwner, { devices ->
+            devices.forEach { device ->
+                lifecycleScope.launch {
+                    withContext(Dispatchers.IO) {
+                        Log.i("SONOS_", "Device : ${device.deviceName}")
+                    }
+                }
+            }
+        })
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
+
     }
 
 }
--- app/src/main/java/com/core/sonoscontroller/ui/main/MainViewModel.kt
+++ app/src/main/java/com/core/sonoscontroller/ui/main/MainViewModel.kt
@@ -1,7 +1,23 @@
 package com.core.sonoscontroller.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.core.sonoscontroller.sonos.SonosDevice
+import com.core.sonoscontroller.sonos.SonosDiscovery
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val devicesLiveData = MutableLiveData<List<SonosDevice>>()
+
+    fun discoverDevices() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val devices = SonosDiscovery.discover()
+                devicesLiveData.postValue(devices)
+            }
+        }
+    }
 }
